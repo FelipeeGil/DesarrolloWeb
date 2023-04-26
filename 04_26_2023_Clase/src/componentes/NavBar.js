872f474d379f0b35525ccca4f8436862a0e4476b
FelipeeGil <73724191+FelipeeGil@users.noreply.github.com>
2023-04-26 11:22:53 -0500

    Clase

---
+++ 04_26_2023_Clase/src/componentes/NavBar.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+export const NavBar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Inicio</Link>
+          </li>
+          <li>
+            <Link to="/Empresa">Empresa</Link>
+          </li>
+          <li>
+            <Link to="/Contacto">Contacto</Link>
+          </li>
+          <li>
+            <Link to="/Conocenos">Conocenos</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
